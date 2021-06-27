c5c3ea0b2f4704338621d669a71e5927e3ecaed7
kirahan <hanzhaoapply@gmail.com>
2021-06-27 22:12:19 +0800

    使用云oss来存储动画

--- src/components/ObsSvgaRender.vue
+++ src/components/ObsSvgaRender.vue
@@ -10,15 +10,6 @@ import svgaplayer from "svgaplayerweb"
 import {useWebsocket} from '../hooks';
 import {useRoute} from 'vue-router'
 
-import svga1 from '../assets/SVGA/1.svga?url'
-import svga2 from '../assets/SVGA/2.svga?url'
-import svga3 from '../assets/SVGA/3.svga?url'
-import svga4 from '../assets/SVGA/4.svga?url'
-import svga5 from '../assets/SVGA/5.svga?url'
-import svga6 from '../assets/SVGA/6.svga?url'
-import svga7 from '../assets/SVGA/7.svga?url'
-import svga8 from '../assets/SVGA/8.svga?url'
-
 export default defineComponent({
     name :"ObsSvgaRender",
     props: {
@@ -27,25 +18,20 @@ export default defineComponent({
         const state = reactive({
             rendersize : {
                 "display": "flex",
-                "justify-content": "center"
+                "justify-content": "flex-start"
             },
         })
         
         const route = useRoute()
     
 
-        const svgaSourceLists = [svga1,svga2,svga3,svga4,svga5,svga6,svga7,svga8]
 
 
         onMounted(()=>{
             let player = new svgaplayer.Player("#Canvas")
             var parser = new svgaplayer.Parser()
             const id = Number(route.params.id)
-             parser.load(svgaSourceLists[id-1], function(videoItem) {
-                console.log(videoItem)
-                // state.rendersize.width = videoItem.videoSize.width + "px"
-                // state.rendersize.height = videoItem.videoSize.height + "px"
-                console.log(state.rendersize)
+             parser.load(`https://obs-public-recourse.oss-cn-beijing.aliyuncs.com/${id}.svga`, function(videoItem) {
                 player.setVideoItem(videoItem);
                 player.loops = 1;
                 player.startAnimation();
